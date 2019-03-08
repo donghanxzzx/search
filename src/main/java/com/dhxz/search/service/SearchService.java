53b267f0a4148fd060cdd68ebd9a446af6e9fd4d
董晗 <handong@alog.com>
2019-03-08 19:25:53 +0800

    修改调用逻辑

--- src/main/java/com/dhxz/search/service/SearchService.java
+++ src/main/java/com/dhxz/search/service/SearchService.java
@@ -54,7 +54,7 @@ public class SearchService {
     private final String topAllVisit = base + "/top-allvisit";
     private final Integer allVisitMaxPage = 741;
 
-    private ExecutorService commonTaskExecutor = Executors.newFixedThreadPool(32,
+    private ExecutorService commonTaskExecutor = Executors.newFixedThreadPool(16,
             new ThreadFactory() {
                 private AtomicInteger counter = new AtomicInteger(0);
 
@@ -67,7 +67,7 @@ public class SearchService {
                     return taskThread;
                 }
             });
-    private ExecutorService contentTaskExecutor = Executors.newFixedThreadPool(32,
+    private ExecutorService contentTaskExecutor = Executors.newFixedThreadPool(64,
             new ThreadFactory() {
                 private AtomicInteger counter = new AtomicInteger(0);
 
