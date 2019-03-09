4ba9adc9caa03d03abf6da16ac89194aeffad54b
donghanxzzx <donghanxzzx@gmail.com>
2019-03-10 03:01:32 +0800

    ``

--- src/main/java/com/dhxz/search/web/utils/ClientUtil.java
+++ src/main/java/com/dhxz/search/web/utils/ClientUtil.java
@@ -24,7 +24,7 @@ public class ClientUtil {
         limiter.acquire();
         Document document = null;
         try {
-            document = Jsoup.connect(url).timeout(5000).header(HttpHeaders.USER_AGENT,
+            document = Jsoup.connect(url).timeout(3000).header(HttpHeaders.USER_AGENT,
                     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
                     .header(HttpHeaders.ACCEPT_LANGUAGE, "zh,zh-CN;q=0.9,en;q=0.8,zh-TW;q=0.7")
                     .get();
